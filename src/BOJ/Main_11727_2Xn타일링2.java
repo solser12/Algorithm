39c4f752cae44d728678e366783c484b373e1e78
박성호 <solser12@naver.com>
2021-08-03 15:19:46 +0900

    [DP]BOJ 11727 2Xn타일링2

---
+++ src/BOJ/Main_11727_2Xn타일링2.java
@@ -0,0 +1,30 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_11727_2Xn타일링2 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+        if (n == 1) {
+            System.out.println(dp[1]);
+        } else {
+            dp[2] = 3;
+            for (int i = 3; i <= n; i++) {
+                dp[i] = (dp[i - 1] + (dp[i - 2] * 2)) % 10007;
+            }
+            System.out.println(dp[n]);
+        }
+
+        br.close();
+    }
+}
