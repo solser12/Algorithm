997b1029c5542e338eb72cdfd65332baa6c9ce4a
solser12 <solser1526@gmail.com>
2021-08-31 19:30:43 +0900

    [BinarySearsh]BOJ 2467 용액

--- src/BOJ/Main_2467_용액.java
+++ src/BOJ/Main_2467_용액.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main_2467_두용액 {
+public class Main_2467_용액 {
 
     public static void main(String[] args) throws IOException {
 
