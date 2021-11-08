d5fa6b74788e55ed7b909c26429280633907e67d
solser12 <solser1526@gmail.com>
2021-11-09 02:22:35 +0900

    [Greedy]BOJ 11047 동전0

---
+++ src/BOJ/Main_11047_동전0.java
@@ -0,0 +1,35 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_11047_동전0 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] coins = new int[N];
+        for (int i = 0; i < N; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+
+        int count = 0;
+        for (int i = N - 1; i >= 0; i--) {
+
+            count += K / coins[i];
+            K %= coins[i];
+
+            if (K == 0) break;
+        }
+
+        System.out.println(count);
+        br.close();
+    }
+}
