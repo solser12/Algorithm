013a504b47020b07023fc41e99f230ff8ac9006e
solser12 <solser1526@gmail.com>
2021-12-20 00:05:15 +0900

    [Greedy]BOJ 2437 저울

---
+++ src/BOJ/Main_2437_저울.java
@@ -0,0 +1,31 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_2437_저울 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int maxNum = 1, index = 0;
+        while (index < N && maxNum >= arr[index]) {
+            maxNum += arr[index++];
+        }
+
+        System.out.println(maxNum);
+        br.close();
+    }
+}
