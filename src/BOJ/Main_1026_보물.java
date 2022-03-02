f5468c6f9c5185683e7888f857274a5b8f2f0398
solser12 <solser1526@gmail.com>
2022-03-03 00:50:35 +0900

    [Math]BOJ 1026 보물

---
+++ src/BOJ/Main_1026_보물.java
@@ -0,0 +1,36 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_1026_보물 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        int[] B = new int[N];
+
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st1.nextToken());
+            B[i] = Integer.parseInt(st2.nextToken());
+        }
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int ans = 0;
+        for (int i = 0; i < N; i++) {
+            ans += A[i] * B[N - i - 1];
+        }
+
+        System.out.println(ans);
+        br.close();
+    }
+}
