0880ed60d3afe38393e3bdfbfae85a210bb95636
solser12 <solser1526@gmail.com>
2022-03-26 01:27:18 +0900

    [Math]BOJ 7894 큰 수

--- src/BOJ/Main_7894_큰수.java
+++ src/BOJ/Main_7894_큰수.java
@@ -11,15 +11,17 @@ public class Main_7894_큰수 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
+        int[] digit = new int[10000001];
+        double sum = 0;
+        for (int i = 1; i <= 10000000; i++) {
+            sum += Math.log10(i);
+            digit[i] = (int)sum + 1;
+        }
+
         int T = Integer.parseInt(br.readLine());
         for (int t = 1; t <= T; t++) {
             int m = Integer.parseInt(br.readLine());
-
-            double sum = 0;
-            for (int i = 2; i <= m; i++) {
-                sum += Math.log10(i);
-            }
-            sb.append((int)sum + 1).append('\n');
+            sb.append(digit[m]).append('\n');
         }
 
         System.out.println(sb);
