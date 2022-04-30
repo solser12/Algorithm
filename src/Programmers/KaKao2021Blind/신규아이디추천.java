8791b1eb7b8e6baec3033fe7017d065500272f4f
solser12 <solser1526@gmail.com>
2022-04-30 21:27:48 +0900

    [String]Programmers 신규 아이디 추천

---
+++ src/Programmers/KaKao2021Blind/신규아이디추천.java
@@ -0,0 +1,76 @@
+package Programmers.KaKao2021Blind;
+
+public class 신규아이디추천 {
+
+    public static void main(String[] args) {
+
+        System.out.println(solution("...!@BaT#*..y.abcdefghijklm"));
+        System.out.println(solution("z-+.^."));
+        System.out.println(solution("=.="));
+        System.out.println(solution("123_.def"));
+        System.out.println(solution("abcdefghijklmn.p"));
+    }
+
+    public static String solution(String new_id) {
+
+        char[] id = new_id.toCharArray();
+        for (int i = 0; i < id.length; i++) {
+            if ('A' <= id[i] && id[i] <= 'Z') {
+                id[i] += 32;
+            }
+
+            if (!checkChar(id[i])) {
+                id[i] = '+';
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (char c : id) {
+            if (c == '+') continue;
+            sb.append(c);
+        }
+        id = sb.toString().toCharArray();
+
+        for (int i = 0; i < id.length - 1; i++) {
+            if (id[i] == '.' && id[i + 1] == '.') {
+                id[i] = '+';
+            }
+        }
+
+        sb = new StringBuilder();
+        for (char c : id) {
+            if (c == '+') continue;
+            sb.append(c);
+        }
+
+        while (sb.length() > 0 && sb.charAt(0) == '.') {
+            sb.delete(0, 1);
+        }
+
+        while (sb.length() > 0 && sb.charAt(sb.length() - 1) == '.') {
+            sb.setLength(sb.length() - 1);
+        }
+
+        if (sb.length() == 0) {
+            sb.append('a');
+        }
+
+        if (sb.length() > 15) {
+            sb.setLength(15);
+        }
+
+        if (sb.length() > 0 && sb.charAt(sb.length() - 1) == '.') {
+            sb.setLength(sb.length() - 1);
+        }
+
+        while (sb.length() < 3) {
+            sb.append(sb.charAt(sb.length() - 1));
+        }
+
+        return sb.toString();
+    }
+
+    public static boolean checkChar(char c) {
+        return ('a' <= c && c <= 'z') || ('0' <= c && c <= '9') || (c == '-') || (c == '.') || (c == '_');
+    }
+}
