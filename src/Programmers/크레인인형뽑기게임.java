3e6088fc2fbe84c84a8635d505f3186cfe9d0762
solser12 <solser1526@gmail.com>
2022-05-12 02:07:44 +0900

    [Stack]Programmers 크레인 인형 뽑기 게임

--- src/Programmers/크레인인형뽑기게임.java
+++ src/Programmers/크레인인형뽑기게임.java
@@ -1,18 +1,10 @@
-package Programmers.kako2019internship;
+package Programmers;
 
 import java.util.Stack;
 
 public class 크레인인형뽑기게임 {
 
-    public static void main(String[] args) {
-
-        int[][] board = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
-        int[] moves = {1,5,3,5,1,2,1,4};
-
-        System.out.println(solution(board, moves));
-    }
-
-    public static int solution(int[][] board, int[] moves) {
+    public int solution(int[][] board, int[] moves) {
 
         Stack<Integer>[] machine = new Stack[board[0].length];
         for (int i = 0; i < board[0].length; i++) {
