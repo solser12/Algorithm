4a8412ad5f7871b4632683c98428478e5bd5e313
solser12 <solser1526@gmail.com>
2022-06-13 01:52:39 +0900

    [Math]Programmers 약수의 개수와 덧셈

--- src/Programmers/약수의개수와덧셈.java
+++ src/Programmers/약수의개수와덧셈.java
@@ -1,7 +1,19 @@
 package Programmers;
 
 public class 약수의개수와덧셈 {
-
     public int solution(int left, int right) {
-        g
+        int ans = 0;
+        for (int i = left; i <= right; i++) {
+            if (squared((int) Math.sqrt(i)) == i) {
+                ans -= i;
+            } else {
+                ans += i;
+            }
+        }
+        return ans;
+    }
+
+    public int squared(int num) {
+        return num * num;
+    }
 }
