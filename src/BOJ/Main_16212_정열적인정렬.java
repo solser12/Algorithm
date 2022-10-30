3c3ad29ba5f39733eef6326fe1fdd44bafefd3e4
solser12 <solser1526@gmail.com>
2022-10-30 23:10:12 +0900

    [Sort]BOJ 16212 정열적인 정렬

---
+++ src/BOJ/Main_16212_정열적인정렬.java
@@ -0,0 +1,31 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_16212_정열적인정렬 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i : arr) {
+            sb.append(i).append(' ');
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
