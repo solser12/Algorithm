efffe4956efb4414616ac29cff76296d828a64a9
solser12 <solser1526@gmail.com>
2022-11-21 02:41:29 +0900

    [Implementation]BOJ 1051 숫자 정사각형

--- src/BOJ/Main_1051_숫자_정사각형.java
+++ src/BOJ/Main_1051_숫자_정사각형.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main_1051_숫자정사각형 {
+public class Main_1051_숫자_정사각형 {
 
     public static void main(String[] args) throws IOException {
 
