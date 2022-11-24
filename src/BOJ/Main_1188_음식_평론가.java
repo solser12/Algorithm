7bc44dbe9ec690e4939b2d4d4a1c830a596240d7
solser12 <solser1526@gmail.com>
2022-11-25 00:30:50 +0900

    [Math]BOJ 1188 음식 평론가

--- src/BOJ/Main_1188_음식_평론가.java
+++ src/BOJ/Main_1188_음식_평론가.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main_1188_음식평론가 {
+public class Main_1188_음식_평론가 {
 
     public static void main(String[] args) throws IOException {
 
