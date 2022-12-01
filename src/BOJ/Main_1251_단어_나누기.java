b2a42374591e60029b82f1e0e364b2016bf31e3b
solser12 <solser1526@gmail.com>
2022-12-01 21:03:20 +0900

    [String]BOJ 1251 단어 나누기

--- src/BOJ/Main_1251_단어_나누기.java
+++ src/BOJ/Main_1251_단어_나누기.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main_1251_단어나누기 {
+public class Main_1251_단어_나누기 {
 
     public static void main(String[] args) throws IOException {
 
