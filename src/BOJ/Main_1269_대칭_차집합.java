dfa6c1b59f15b40dde844ca2e7510b3fad7ce75d
solser12 <solser1526@gmail.com>
2022-12-02 02:22:05 +0900

    [HashMap]BOJ 1269 대친 차집합

--- src/BOJ/Main_1269_대칭_차집합.java
+++ src/BOJ/Main_1269_대칭_차집합.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.StringTokenizer;
 
-public class Main_1269_대칭차집합 {
+public class Main_1269_대칭_차집합 {
 
     public static void main(String[] args) throws IOException {
 
