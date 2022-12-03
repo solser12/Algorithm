118bda1a6f9c019c48f726f607b6296e485e7eea
solser12 <solser1526@gmail.com>
2022-12-04 01:23:53 +0900

    [String]BOJ 1152 단어의 개수

---
+++ src/BOJ/Main_1152_단어의_개수.java
@@ -0,0 +1,19 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_1152_단어의_개수 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        System.out.println(st.countTokens());
+
+        br.close();
+    }
+}
