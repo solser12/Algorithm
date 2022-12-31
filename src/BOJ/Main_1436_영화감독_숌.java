d7856ac8beb8ac83b94fd73ecffc92b176645882
solser12 <solser1526@gmail.com>
2023-01-01 00:25:52 +0900

    [Brute Force]BOJ 1436 영화감독 숌

--- src/BOJ/Main_1436_영화감독_숌.java
+++ src/BOJ/Main_1436_영화감독_숌.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main_1436_영화감독숌 {
+public class Main_1436_영화감독_숌 {
 
     public static void main(String[] args) throws IOException {
 
