b5f20eb0dc8a3e878adc1e7a7e5d77a9caaa1fc5
solser12 <solser1526@gmail.com>
2023-01-09 23:01:12 +0900

    [Sort] BOJ 2750 수 정렬하기

---
+++ src/BOJ/Main_2750_수_정렬하기.java
@@ -0,0 +1,29 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main_2750_수_정렬하기 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        for (int i : arr) {
+            sb.append(i).append('\n');
+        }
+
+        System.out.print(sb);
+        br.close();
+    }
+}
